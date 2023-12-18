52c914b76d83bf9e971c03a3f2dcb4a020ac6873
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-12-18 16:01:43 -0500

    Created logo component

--- face-recognition-app/src/App.js
+++ face-recognition-app/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
 function App() {
   return (
     <div className="App">

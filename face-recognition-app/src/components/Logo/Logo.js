b5a352bd8b67abb48000be03999024b20d7efeda
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-12-18 16:32:33 -0500

    Added sample logo to logo component

--- face-recognition-app/src/components/Logo/Logo.js
+++ face-recognition-app/src/components/Logo/Logo.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
+import brain from "./brain.png";
+import "./Logo.css";
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-      <Tilt
-        className="Tilt br2 shadow-2"
-        options={{ max: 25 }}
-        style={{ height: 150, width: 150 }}
-      >
-        <div className="Tilt-inner">
-          <h1>👀</h1>
+      <Tilt className="Tilt br2 shadow-2" style={{ height: 150, width: 150 }}>
+        <div className="Tilt-inner pa2">
+          <img style={{ paddingTop: "5px" }} alt="logo" src={brain}></img>
         </div>
       </Tilt>
     </div>

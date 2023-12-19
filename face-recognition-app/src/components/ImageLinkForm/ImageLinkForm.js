5e7c06ad937e19f524b539a3785f4952ce3311b6
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-12-18 22:29:46 -0500

    Added input form

--- face-recognition-app/src/components/ImageLinkForm/ImageLinkForm.js
+++ face-recognition-app/src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,6 +5,12 @@ const ImageLinkForm = () => {
       <p className="f3">
         {"This magic brain will detect faces in your pictures. Give it a try."}
       </p>
+      <div>
+        <input className="f4 pa2 w-70 center" type="text" />
+        <button className="w-e0 grow f4 link ph3 pv2 dib white bg-light-purple">
+          Detect
+        </button>
+      </div>
     </div>
   );
 };

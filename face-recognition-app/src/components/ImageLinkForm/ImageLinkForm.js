3b4eef80119b21906a3a96777a4ca81fbb1e126b
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-12-18 22:26:53 -0500

    Created image link form component

---
+++ face-recognition-app/src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+const ImageLinkForm = () => {
+  return (
+    <div>
+      <p className="f3">
+        {"This magic brain will detect faces in your pictures. Give it a try."}
+      </p>
+    </div>
+  );
+};
+
+export default ImageLinkForm;

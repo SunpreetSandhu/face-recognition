4a8ba0c42919394a9aa5715f94c02fe30181f829
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-12-20 15:00:20 -0500

    Console output for text input

--- face-recognition-app/src/App.js
+++ face-recognition-app/src/App.js
@@ -7,6 +7,16 @@ import ParticlesBg from "particles-bg";
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
   render() {
     return (
       <div className="App">
@@ -14,7 +24,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
         {/* {<Logo />
         <ImageLinkForm />
         <FaceRecognition />} */}

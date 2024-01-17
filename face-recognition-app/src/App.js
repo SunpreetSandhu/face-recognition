2a8619b907c0ee7e2d88cd5a4dc3dca9344e5b4b
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-01-17 11:36:24 -0500

    Used image put endpoint to update entry count for user

--- face-recognition-app/src/App.js
+++ face-recognition-app/src/App.js
@@ -138,9 +138,24 @@ class App extends Component {
       returnClarifaiJSONRequest(this.state.input)
     )
       .then((response) => response.json())
-      .then((response) =>
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      .then((response) => {
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+
       .catch((err) => console.log(err));
 
     //   .then((response) => {
